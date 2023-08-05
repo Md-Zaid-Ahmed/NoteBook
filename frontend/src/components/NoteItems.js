316c0b5ad026aaafd29ec2422ea6ceff67f52c64
unknown <za6785583@gmail.com>
2023-08-05 21:51:38 +0530

    second-commit

--- frontend/src/components/NoteItems.js
+++ frontend/src/components/NoteItems.js
@@ -13,7 +13,7 @@ function NoteItems(props) {
 
 
         // alert(props.id)
-        axios.post('http://localhost:8000/delete', {
+        axios.post('https://notebookbackend-xr2m.onrender.com/delete', {
             user: props.id,
             tag: props.tag,
             description: props.description,
